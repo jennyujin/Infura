47a541a8b18c96db0382bb020bbf338b7de0bcd7
jennyujin <eppunijyj@gmail.com>
2023-11-08 17:50:54 +0900

    etherscan api

---
+++ etherscan.py
@@ -0,0 +1,15 @@
+from requests import get
+import os
+from dotenv import load_dotenv
+
+# .env 파일을 로드함
+load_dotenv()
+api_key = os.environ['API_KEY']
+
+etherscan_api = api_key
+targettime = 1664550000 # 2022-10-01 00:00:00
+blocknum_url = f'https://api.etherscan.io/api?module=block&action=getblocknobytime&timestamp={targettime}&closest=before&apikey={etherscan_api}'
+
+response = get(blocknum_url)
+data = response.json()
+print(data)
